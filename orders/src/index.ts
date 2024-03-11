78dbe2d62b23f7a424557817417467b2a1d8553d
Valerio Mattera <matteravalerio@gmail.com>
2024-03-11 14:16:22 +0000

    Added ExpirationCompleteListener in Orders ms

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import { app } from './app'
 import { natsWrapper } from './nats-wrapper'
 import { TicketUpdatedListener } from './listeners/ticket-updated-listener'
 import { TicketCreatedListener } from './listeners/ticket-created-listener'
+import { ExpirationCompleteListener } from './listeners/expiration-complete-listener'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -41,6 +42,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen()
     new TicketUpdatedListener(natsWrapper.client).listen()
+    new ExpirationCompleteListener(natsWrapper.client).listen()
 
     await mongoose.connect(process.env.MONGO_URI)
     console.log('Connected to MongoDB')
---
+++ orders/src/listeners/expiration-complete-listener.ts
@@ -0,0 +1,39 @@
+import { Message } from 'node-nats-streaming'
+import {
+  Listener,
+  Subjects,
+  ExpirationCompleteEvent,
+  NotFoundError,
+  OrderStatus,
+} from '@valeriom91-org/common'
+import { Order } from '../models/order'
+import { queueGroupName } from './queue-group-name'
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher'
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationComplete
+  queueGroupName = queueGroupName
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId)
+
+    if (!order) {
+      throw new NotFoundError('Order not found')
+    }
+
+    order.set({ status: OrderStatus.Cancelled })
+
+    await order.save()
+
+    // Emit an event saying this order was cancelled!
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    })
+
+    msg.ack()
+  }
+}
